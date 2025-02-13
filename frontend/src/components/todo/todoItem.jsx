8d010c1113e423d67aaae71888925ee7c39b2b8a
bhavya92 <bhavyasharma535@gmail.com>
2025-02-13 14:07:17 +0530

    delete todo Functionality

--- frontend/src/components/todo/todoItem.jsx
+++ frontend/src/components/todo/todoItem.jsx
@@ -1,18 +1,59 @@
 import LoopRoundedIcon from "@mui/icons-material/LoopRounded";
 import StarRoundedIcon from "@mui/icons-material/StarRounded";
-import { useContext } from "react";
+import { useContext, useEffect,useState } from "react";
 import { DetailSidebarContext } from "../../context/detailBar";
 import { TodoContext } from "../../context/todoContext";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { deleteTodo } from "../../services/todo";
 
 export default function TodoItem({ singleTodo }) {
 
   const { setDetailBarContent, setIsDetailVisible  } = useContext(DetailSidebarContext);
-  const { setTodoInDetail  } = useContext(TodoContext);
+  const { setTodoInDetail,todo,setTodo  } = useContext(TodoContext);
+  const [isStarred, setIsStarred] = useState(null);
+  const [ repeat, setRepeat ] = useState(null);
+  
+  useEffect( ()=>{
+    setIsStarred(singleTodo.starred);
+    setRepeat(singleTodo.daily);
+  },[])
 
   function displayTodoHandler() {
     setDetailBarContent('todo');
     setTodoInDetail(singleTodo);
     setIsDetailVisible(true);
+  }
+
+  async function deleteTodoHandler(e){
+    e.stopPropagation();
+    console.log("deleting todo")
+    console.log("original todo");
+    console.log(todo);
+    const response = await deleteTodo(singleTodo._id);
+    if(response.status === '200') {
+      //show alert
+      //update todo array
+      //let newTodos = todo.map( (item) => {item} )
+      let newTodo = todo.map((list) => ({
+        ...list,
+        data : list.data.filter( (item) => 
+            item._id !== singleTodo._id)
+      }));
+      console.log(newTodo)
+      setTodo(newTodo);
+    } else {
+      setSeverity("error");
+      setAlertMessage("Error deleting todo.");
+      setIsAlert(true);
+    }
+  }
+
+  function starTodoHandler(){
+
+  }
+
+  function repeatTodoHandler(){
+
   }
 
   return (
@@ -29,11 +70,17 @@ export default function TodoItem({ singleTodo }) {
       <div className="grow m-2 text-base text-ellipsis font-neue font-medium overflow-hidden whitespace-nowrap">
         <span>{singleTodo.title} </span>
       </div>
-      <div className="transition-all duration-200 ease-in-out flex-none m-2 w-fit h-fit  cursor-pointer hover:scale-110 ">
-        <StarRoundedIcon sx={{ color: "#292524", fontSize: 20 }} />
+      <div className="transition-all duration-200 ease-in-out flex-none m-2 w-fit h-fit  cursor-pointer hover:scale-110 "
+          >
+        <StarRoundedIcon sx={{ color: `${isStarred ? "#b08f26" : "#292524"}`, fontSize: 20 }} />
       </div>
       <div className="transition-all duration-200 ease-in-out flex-none m-2 w-fit h-fit  cursor-pointer hover:scale-110 ">
-        <LoopRoundedIcon sx={{ color: "#292524", fontSize: 20 }} />
+        <LoopRoundedIcon sx={{ color: `${repeat ? "#008000" : "#292524"}`, fontSize: 20 }} />
+      </div>
+      <div className="transition-all duration-200 ease-in-out flex-none m-2 w-fit h-fit  cursor-pointer hover:scale-110 "
+           onClick={(e) => deleteTodoHandler(e)}      
+      >
+        <DeleteIcon sx={{ color: "#AA4A44", fontSize: 20 }} />
       </div>
     </div>
   );
